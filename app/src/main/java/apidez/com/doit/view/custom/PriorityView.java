3588dc1a5229d65fe8f3d6557cbadd2c9f8e6123
Quan Vu <nongdenchet8435848@gmail.com>
2016-02-11 10:46:32 +0700

    add priority picker

--- app/src/main/java/apidez/com/doit/view/custom/PriorityView.java
+++ app/src/main/java/apidez/com/doit/view/custom/PriorityView.java
@@ -2,6 +2,7 @@ package apidez.com.doit.view.custom;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
@@ -15,39 +16,60 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * Created by nongdenchet on 2/8/16.
  */
 public class PriorityView extends RelativeLayout {
-    private TextView mTitle;
-    private CircleImageView mBackground;
+    private int mTitleColor = R.color.bg_priority_high;
     private boolean isChecked = false;
-
-    public PriorityView(Context context) {
-        super(context);
-        initialize();
-    }
+    private boolean isSelected = true;
+    private TextView mTitle;
+    private CircleImageView mBackgroundDisable;
+    private CircleImageView mBackground;
 
     public PriorityView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initialize();
+        initialize(attrs);
     }
 
     public PriorityView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initialize();
+        initialize(attrs);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public PriorityView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        initialize();
+        initialize(attrs);
     }
 
-    private void initialize() {
+    private void initialize(AttributeSet attrs) {
         inflate(getContext(), R.layout.priority_view, this);
         initViews();
+        initStyles(attrs);
+    }
+
+    private void initStyles(AttributeSet attrs) {
+        TypedArray attributes = getContext().obtainStyledAttributes(attrs, R.styleable.PriorityView);
+        setTitle(attributes.getString(R.styleable.PriorityView_priorityTitle));
+        setPriorityColor(attributes.getResourceId(R.styleable.PriorityView_priorityColor, R.color.bg_priority_high));
+        setPriorityTitleColor(attributes.getResourceId(R.styleable.PriorityView_priorityTitleColor, android.R.color.white));
+        attributes.recycle();
     }
 
     private void initViews() {
         mTitle = (TextView) findViewById(R.id.title);
         mBackground = (CircleImageView) findViewById(R.id.background);
+    }
+
+    public void select() {
+        mTitle.setTextColor(ContextCompat.getColor(getContext(), mTitleColor));
+        mBackground.setVisibility(VISIBLE);
+        mBackgroundDisable.setVisibility(GONE);
+        isSelected = true;
+    }
+
+    public void unSelect() {
+        mTitle.setTextColor(ContextCompat.getColor(getContext(), android.R.color.black));
+        mBackground.setVisibility(GONE);
+        mBackgroundDisable.setVisibility(VISIBLE);
+        isSelected = false;
     }
 
     public void setPriorityTitleColor(int color) {
